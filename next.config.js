f039304324da90a30c87df5fa27f72a019c967b4
Alstha <alsonstha987@gmail.com>
2025-06-13 11:16:21 +0545

    fix: use images.remotePatterns for ibb.co and web.whatsapp.com in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['web.whatsapp.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ibb.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'web.whatsapp.com',
+      },
+    ],
   },
   /* config options here */
 };
